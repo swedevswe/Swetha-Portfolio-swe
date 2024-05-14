1564bbb0406ecf26ef8b35d061337d7502101c4d
Swetha Sebastian <swthsebastian@gmail.com>
2024-05-14 10:48:51 +0100

    Updates

---
+++ precache-manifest.b3c38819c6b99cb0872d926382c1c956.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef44fe29811190f2b6598fce69e09435",
+    "url": "/Swetha-Portfolio-swe/index.html"
+  },
+  {
+    "revision": "a404ca5e4d2f4cfcae8d",
+    "url": "/Swetha-Portfolio-swe/static/css/2.b8e7cbba.chunk.css"
+  },
+  {
+    "revision": "c73e7f064c8928b51c21",
+    "url": "/Swetha-Portfolio-swe/static/css/main.f2c16aec.chunk.css"
+  },
+  {
+    "revision": "a404ca5e4d2f4cfcae8d",
+    "url": "/Swetha-Portfolio-swe/static/js/2.65489c55.chunk.js"
+  },
+  {
+    "revision": "5f970f35d1b21881415c",
+    "url": "/Swetha-Portfolio-swe/static/js/3.435f9e5a.chunk.js"
+  },
+  {
+    "revision": "c73e7f064c8928b51c21",
+    "url": "/Swetha-Portfolio-swe/static/js/main.e5136798.chunk.js"
+  },
+  {
+    "revision": "9a61f0dea772faaf29ae",
+    "url": "/Swetha-Portfolio-swe/static/js/runtime~main.a83829e1.js"
+  },
+  {
+    "revision": "abd18e548910aee453f8537124eb2874",
+    "url": "/Swetha-Portfolio-swe/static/media/CentraNo2-Bold.abd18e54.ttf"
+  },
+  {
+    "revision": "090b88d28af7652bab33cb15566c58f5",
+    "url": "/Swetha-Portfolio-swe/static/media/CentraNo2-Book.090b88d2.ttf"
+  },
+  {
+    "revision": "180408241ad7ff00b6cea86ee805fec8",
+    "url": "/Swetha-Portfolio-swe/static/media/CentraNo2-Medium.18040824.ttf"
+  },
+  {
+    "revision": "91837c3008b520c0dfc0fc76454a9139",
+    "url": "/Swetha-Portfolio-swe/static/media/banner-bg.91837c30.png"
+  },
+  {
+    "revision": "50663b2b6a0fa3905c7f7aadd9ca1fd5",
+    "url": "/Swetha-Portfolio-swe/static/media/color-sharp.50663b2b.png"
+  },
+  {
+    "revision": "1173d6aa8f8bf1766978eda50cbd4c7b",
+    "url": "/Swetha-Portfolio-swe/static/media/color-sharp2.1173d6aa.png"
+  },
+  {
+    "revision": "bf09516b7a040f86b5d16b45a195d4aa",
+    "url": "/Swetha-Portfolio-swe/static/media/contact-img.bf09516b.svg"
+  },
+  {
+    "revision": "13c154355bb09606ef2052b6aacd5fc0",
+    "url": "/Swetha-Portfolio-swe/static/media/footer-bg.13c15435.png"
+  },
+  {
+    "revision": "e02c229832d776d895be48c40af8cdbc",
+    "url": "/Swetha-Portfolio-swe/static/media/header-img.e02c2298.svg"
+  },
+  {
+    "revision": "cd2c56e97e6a36b14e1effec171abc4b",
+    "url": "/Swetha-Portfolio-swe/static/media/logo.cd2c56e9.svg"
+  },
+  {
+    "revision": "4737ddea78d77e2e0251ffc7da95a015",
+    "url": "/Swetha-Portfolio-swe/static/media/meter1.4737ddea.svg"
+  },
+  {
+    "revision": "18cd06afd1dd4558dfbf4d5a528aa640",
+    "url": "/Swetha-Portfolio-swe/static/media/meter2.18cd06af.svg"
+  },
+  {
+    "revision": "13a2ff8dc08442981a7a1e28dc50ee17",
+    "url": "/Swetha-Portfolio-swe/static/media/meter3.13a2ff8d.svg"
+  },
+  {
+    "revision": "cfcf62ae43316fb0b099763517f0153c",
+    "url": "/Swetha-Portfolio-swe/static/media/nav-icon1.cfcf62ae.svg"
+  },
+  {
+    "revision": "974d1ea0eb5fe0830ebbb9a8e68a4c6d",
+    "url": "/Swetha-Portfolio-swe/static/media/nav-icon2.974d1ea0.svg"
+  },
+  {
+    "revision": "ad703e74a8c49bb111feb4b6cd9dfccd",
+    "url": "/Swetha-Portfolio-swe/static/media/nav-icon3.ad703e74.svg"
+  },
+  {
+    "revision": "c3188ccd40977075faf950466471c2a8",
+    "url": "/Swetha-Portfolio-swe/static/media/project-img1.c3188ccd.png"
+  },
+  {
+    "revision": "6e3a3867401097d2319805751c9f439b",
+    "url": "/Swetha-Portfolio-swe/static/media/project-img2.6e3a3867.png"
+  },
+  {
+    "revision": "9796734d864c0027849294deb7b92826",
+    "url": "/Swetha-Portfolio-swe/static/media/project-img3.9796734d.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Swetha-Portfolio-swe/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Swetha-Portfolio-swe/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Swetha-Portfolio-swe/static/media/revicons.2feb69cc.eot"
+  }
+]);
